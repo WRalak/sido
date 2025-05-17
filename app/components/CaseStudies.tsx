ca30d16a9bde77335ec64898790c25d196b4730b
WRalak <your-noreply-email@users.noreply.github.com>
2025-05-17 12:36:29 +0300

    minor changes

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -6,13 +6,15 @@ import Tuks from './Tuks';
 
 const CaseStudies = () => {
   return (
-    <div className="px-3 py-6 ">
-      <h2 className="text-[18px] text-gray-900 font-extrabold md:px-8 lg:px-16 xl:px-68 2xl:px-72   mb-4">Case Studies</h2>
-      <div className="flex overflow-x-auto  whitespace-nowrap custom-scroll">
-        <div className="inline-block min-w-[360px]"><Gap /></div>
-        <div className="inline-block min-w-[360px]"><Sol /></div>
-        <div className="inline-block min-w-[360px]"><Cars /></div>
-        <div className="inline-block min-w-[360px]"><Tuks /></div>
+    <div className="px-4 py-6 mx-auto max-w-screen-2xl">
+      <div className="w-full md:px-8 lg:px-16 xl:px-20 2xl:px-0">
+        <h2 className="text-[18px] xl:px-44 2xl:px-70 text-gray-900 font-extrabold mb-4">Case Studies</h2>
+        <div className="flex overflow-x-auto whitespace-nowrap custom-scroll">
+          <div className="inline-block min-w-[360px]"><Gap /></div>
+          <div className="inline-block min-w-[360px]"><Sol /></div>
+          <div className="inline-block min-w-[360px]"><Cars /></div>
+          <div className="inline-block min-w-[360px]"><Tuks /></div>
+        </div>
       </div>
     </div>
   );
